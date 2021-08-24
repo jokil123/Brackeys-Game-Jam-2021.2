7f4e82b51caf8f87d8c222f9c17cc0e6ff7cd582
jo_kil <jo.kil.plays@gmail.com>
2021-08-24 19:46:31 +0200

    Added Bumber Logic

--- PinBall/Assets/Scripts/BouncePad.cs
+++ PinBall/Assets/Scripts/BouncePad.cs
@@ -1,38 +1,86 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class BouncePad : MonoBehaviour, ChildTriggerEnter, ChildTriggerExit
 {
     public bool bounceEnabled;
     public float bounceDelay;
+    public float bounceCooldown;
     public BounceType bounceType;
 
     public Vector3 bounceDirection;
     public float bounceStrengthMultiplier;
 
-    private Collider bounceTrigger;
+    public bool bounceSeperately;
 
 
-    // Start is called before the first frame update
-    void Start()
+    private float LastBounce;
+
+    private HashSet<GameObject> collidedList = new HashSet<GameObject>();
+    private Dictionary<GameObject, float> collidedListTime = new Dictionary<GameObject, float>();
+
+
+    void FixedUpdate()
     {
-        bounceTrigger = gameObject.transform.Find("BounceTrigger").GetComponent<Collider>();
+        HashSet<GameObject> toBounce = new HashSet<GameObject>();
+
+        if (Time.time - LastBounce >= bounceCooldown)
+        {
+            foreach (GameObject collidedObject in collidedList)
+            {
+                if (Time.time - collidedListTime[collidedObject] >= bounceDelay)
+                {
+                    toBounce.Add(collidedObject);
+                }
+            }
+
+            if (bounceSeperately)
+            {
+                Bounce(toBounce.ToList());
+            }
+            else
+            {
+                if (toBounce.Count > 0)
+                {
+                    Bounce(collidedList.ToList());
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void Bounce(List<GameObject> bounceObjects)
     {
-        
+        if (bounceEnabled)
+        {
+            LastBounce = Time.time;
+            foreach (GameObject bounceObject in bounceObjects)
+            {
+                bounceObject.GetComponent<Rigidbody>().AddForce(GetLaunchVector());
+            }
+        }
     }
+
+    Vector3 GetLaunchVector()
+    {
+        return gameObject.transform.rotation * bounceDirection * bounceStrengthMultiplier;
+
+    }
+
+
 
     public void OnTriggerEnterChild(Collider childCollider, Collider other)
     {
+        collidedList.Add(other.gameObject);
+        collidedListTime.Add(other.gameObject, Time.time);
         Debug.LogWarning("Child Enter");
     }
 
     public void OnTriggerExitChild(Collider childCollider, Collider other)
     {
+        collidedList.Remove(other.gameObject);
+        collidedListTime.Remove(other.gameObject);
         Debug.LogWarning("Child Exit");
     }
 
@@ -40,7 +88,7 @@ public class BouncePad : MonoBehaviour, ChildTriggerEnter, ChildTriggerExit
     {
         if (bounceType == BounceType.directional) {
             Vector3 start = gameObject.transform.position;
-            Vector3 end = start + bounceDirection * bounceStrengthMultiplier;
+            Vector3 end = start + GetLaunchVector();
             Gizmos.color = Color.red;
             Gizmos.DrawLine(start, end);
         }
