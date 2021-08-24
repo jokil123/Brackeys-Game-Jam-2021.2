6596b75bed81f1f9b8b7e486a52a4f77f9d39313
jo_kil <jo.kil.plays@gmail.com>
2021-08-24 20:21:18 +0200

    Added Config, and objectified Bounce Pad

--- PinBall/Assets/Scripts/BouncePad.cs
+++ PinBall/Assets/Scripts/BouncePad.cs
@@ -3,58 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class BouncePad : MonoBehaviour, ChildTriggerEnter, ChildTriggerExit
+public class BouncePad : TriggerPad
 {
-    public bool bounceEnabled;
-    public float bounceDelay;
-    public float bounceCooldown;
-    public BounceType bounceType;
-
     public Vector3 bounceDirection;
     public float bounceStrengthMultiplier;
+    public BounceType bounceType;
 
-    public bool bounceSeperately;
-
-
-    private float LastBounce;
-
-    private HashSet<GameObject> collidedList = new HashSet<GameObject>();
-    private Dictionary<GameObject, float> collidedListTime = new Dictionary<GameObject, float>();
-
-
-    void FixedUpdate()
+    public override void TriggerAction(List<GameObject> bounceObjects)
     {
-        HashSet<GameObject> toBounce = new HashSet<GameObject>();
-
-        if (Time.time - LastBounce >= bounceCooldown)
+        if (triggerEnabled)
         {
-            foreach (GameObject collidedObject in collidedList)
-            {
-                if (Time.time - collidedListTime[collidedObject] >= bounceDelay)
-                {
-                    toBounce.Add(collidedObject);
-                }
-            }
-
-            if (bounceSeperately)
-            {
-                Bounce(toBounce.ToList());
-            }
-            else
-            {
-                if (toBounce.Count > 0)
-                {
-                    Bounce(collidedList.ToList());
-                }
-            }
-        }
-    }
-
-    void Bounce(List<GameObject> bounceObjects)
-    {
-        if (bounceEnabled)
-        {
-            LastBounce = Time.time;
+            lastTriggered = Time.time;
             foreach (GameObject bounceObject in bounceObjects)
             {
                 bounceObject.GetComponent<Rigidbody>().AddForce(GetLaunchVector());
@@ -70,23 +29,10 @@ public class BouncePad : MonoBehaviour, ChildTriggerEnter, ChildTriggerExit
 
 
 
-    public void OnTriggerEnterChild(Collider childCollider, Collider other)
-    {
-        collidedList.Add(other.gameObject);
-        collidedListTime.Add(other.gameObject, Time.time);
-        Debug.LogWarning("Child Enter");
-    }
-
-    public void OnTriggerExitChild(Collider childCollider, Collider other)
-    {
-        collidedList.Remove(other.gameObject);
-        collidedListTime.Remove(other.gameObject);
-        Debug.LogWarning("Child Exit");
-    }
-
     private void OnDrawGizmos()
     {
-        if (bounceType == BounceType.directional) {
+        if (bounceType == BounceType.directional)
+        {
             Vector3 start = gameObject.transform.position;
             Vector3 end = start + GetLaunchVector() * Config.GizmoVectorLengthMultiplier;
             Gizmos.color = Color.red;
@@ -97,8 +43,8 @@ public class BouncePad : MonoBehaviour, ChildTriggerEnter, ChildTriggerExit
 
 public enum BounceType
 {
-    radial,
     directional,
+    radial,
     reflect,
     random
 }
--- PinBall/Assets/Scripts/Config.cs
+++ PinBall/Assets/Scripts/Config.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Config
+public static class Config
 {
     public static float GizmoVectorLengthMultiplier = 0.01f;
 }
