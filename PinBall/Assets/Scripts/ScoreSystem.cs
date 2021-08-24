e68deedf43429f225034d9940736d53c63b682ec
Cinnazeyy <imcinnazeyy@gmail.com>
2021-08-25 01:09:38 +0200

    Made basics of Score System

---
+++ PinBall/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField]
+    private Text txtScore;
+    [SerializeField]
+    private Text txtHighScore;
+    
+    private int _score;
+    public int Score
+    {
+        get { return _score; }
+        set { 
+            _score = value;
+            txtHighScore.text = $"Score: {value}";
+            if (value > HighScore)
+            {
+                HighScore = value;
+            }
+        }
+    }
+
+    private int _highScore;
+
+    public int HighScore
+    {
+        get { return _highScore; }
+        set {
+            _highScore = value;
+            txtHighScore.text = $"High Score: {value}";
+        }
+    }
+}
