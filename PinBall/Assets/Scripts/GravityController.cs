d0005fddfbce173a1c36e91a758d3028d4ef721a
jo_kil <jo.kil.plays@gmail.com>
2021-08-25 02:46:38 +0200

    Added gravity controller

---
+++ PinBall/Assets/Scripts/GravityController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityController : MonoBehaviour
+{
+    public Vector3 gravityDirection = new Vector3(0, -1, 0);
+
+    public float gravityStrength = 9.81f;
+
+    public bool showGravityArrow;
+
+    void Awake()
+    {
+        Physics.gravity = gravityDirection.normalized * gravityStrength;
+        Debug.Log(Physics.gravity.y);
+    }
+
+    void OnDrawGizmos()
+    { 
+        if (showGravityArrow)
+        {
+            Vector3 start = gameObject.transform.position;
+            Vector3 end = start + gravityDirection.normalized * gravityStrength;
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(start, end);
+        }
+    }
+}
