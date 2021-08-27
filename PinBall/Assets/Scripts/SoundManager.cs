a47d9bd83adea88117f47d724b1abee483ccd5b2
jo_kil <jo.kil.plays@gmail.com>
2021-08-28 01:59:44 +0200

    stuff + audio

---
+++ PinBall/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField]
+    public List<AudioSource> audioClips = new List<AudioSource>();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+[Serializable]
+public class AudioSource
+{
+    public string name;
+    public AudioClip audioClip;
+}
