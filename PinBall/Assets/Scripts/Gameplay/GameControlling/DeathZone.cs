ecccfb993b2171eb1327fe0cafff067cba174b28
jo_kil <jo.kil.plays@gmail.com>
2021-08-29 06:03:40 +0200

    aah stuff that I couldn't push for the last 2 hours

--- PinBall/Assets/Scripts/Gameplay/GameControlling/DeathZone.cs
+++ PinBall/Assets/Scripts/Gameplay/GameControlling/DeathZone.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class DeathZone : MonoBehaviour
 {
@@ -8,11 +9,19 @@ public class DeathZone : MonoBehaviour
     public HealthSystem healthSystem;
     public int damage = 10;
 
+    public GameObject ParticleEffect;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Ball")
         {
             GameObject.Destroy(other.gameObject);
+
+            if (ParticleEffect)
+            {
+                ParticleEffect.GetComponent<VisualEffect>().Play();
+            }
+
             if (spawnSystem.gameIsRunning)
             {
                 healthSystem.Health -= 100;
